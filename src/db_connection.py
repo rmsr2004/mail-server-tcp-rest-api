3143af59e5e7656f19d3a1659d9413bcac5fe64e
rmsr2004 <rodrigomiguelsr2004@gmail.com>
2024-06-07 00:17:30 +0100

    First Commit: function to connect do db

---
+++ src/db_connection.py
@@ -0,0 +1,14 @@
+import psycopg2
+##
+##  Connect to the database
+##
+def db_connection(config_vars):
+    db = psycopg2.connect(
+        user = config_vars['db_user'],
+        password = config_vars['db_password'],
+        host = config_vars['db_host'],
+        port = config_vars['db_port'],
+        database = config_vars['db_name']
+    )
+    db.autocommit = False
+    return db
